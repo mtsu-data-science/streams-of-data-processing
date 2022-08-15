9bce55f89d0b299f1a8b09ae00b24e3c70e353a8
Alexander Antonison <adantonison@gmail.com>
2022-08-15 14:48:34 -0500

    Add class

--- src/OrganizationMsds.py
+++ src/OrganizationMsds.py
@@ -1,3 +1,6 @@
+
+import os
+
 class OrganizationMsds:
     def __init__(self):
-        self.organization_msds = []
+        self.base_s3_bucket = f"s3://{os.environ['infra']}-mtsu-msds-data-lake-source/sensor-files/"
